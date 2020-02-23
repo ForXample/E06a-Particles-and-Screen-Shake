440df69f5e109ca79ef4a4f94f14249d6a701ef3
Zixi Liu <59849551+ForXample@users.noreply.github.com>
2020-02-23 18:10:00 -0500

    Zixi Liu C220

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -6,4 +6,13 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	if Input.is_action_just_pressed("player_left"):
+		$Camera.add_trauma(3)
+	if Input.is_action_just_pressed("player_right"):
+		$Camera.add_trauma(3)
+	if Input.is_action_just_pressed("player_up"):
+		$Camera.add_trauma(3)
+	if Input.is_action_just_pressed("player_down"):
+		$Camera.add_trauma(3)
 	pass
+	
